ccbcc9723a930cbf861bb6f796bc802b21a2af50
bernatDiaz <bernatdiaz94@gmail.com>
2021-09-23 18:29:26 +0200

    Add files via upload

---
+++ shape_calculator.py
@@ -0,0 +1,43 @@
+class Rectangle:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+  def set_width(self, width):
+    self.width = width
+  def set_height(self, height):
+    self.height = height
+  def get_area(self):
+    return self.width * self.height
+  def get_perimeter(self):
+    return 2 * self.width + 2 * self.height
+  def get_diagonal(self):
+    return (self.width ** 2 + self.height ** 2) ** .5
+  def get_picture(self):
+    if self.width > 50 or self.height > 50:
+      return "Too big for picture."
+    result = ""
+    for i in range(self.height):
+      result = result + "*"*self.width + "\n"
+    #result = result + "*"*self.width
+    return result
+  def get_amount_inside(self, rectangle):
+    return (self.width // rectangle.width) * (self.height // rectangle.height)
+
+  def __str__(self):
+    return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+
+class Square(Rectangle):
+  def __init__(self, side):
+    self.width = side
+    self.height = side
+  def set_side(self, side):
+    self.width = side
+    self.height = side
+  def set_width(self, width):
+    self.width = width
+    self.height = width
+  def set_height(self, height):
+    self.width = height
+    self.height = height
+  def __str__(self):
+    return "Square(side=" + str(self.width) + ")"
